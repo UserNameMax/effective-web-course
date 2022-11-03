d348a39df43290794995220f8e48bad5a596c75b
Maksim Mishenko <sirmaksim47@gmail.com>
2022-11-03 15:40:42 +0600

    [*] reset timer when switch image

--- Lab2/slider/script.js
+++ Lab2/slider/script.js
@@ -1,33 +1,37 @@
-let count = localStorage.getItem('count')??0;
-document.getElementById("img").src = "images//img"+count+".jpg";
-
-setInterval(nextImage,5000);
+let count = localStorage.getItem("count") ?? 0;
+let timer = setInterval(nextImage, 5000);
+document.getElementById("img").src = "images//img" + count + ".jpg";
 
 document.getElementById("d1").onclick = lastImage;
 
-function lastImage(){
-	count = (count + 2) % 3;
-	document.getElementById("img").src = "images//img"+count+".jpg";
+function lastImage() {
+  count = (count + 2) % 3;
+  document.getElementById("img").src = "images//img" + count + ".jpg";
+  clearInterval(timer);
+  timer = setInterval(nextImage, 5000);
 }
 
 document.getElementById("d2").onclick = nextImage;
 
-function nextImage(){
-	count = (count + 1) % 3;
-	document.getElementById("img").src = "images//img"+count+".jpg";
+function nextImage() {
+  count = (count + 1) % 3;
+  document.getElementById("img").src = "images//img" + count + ".jpg";
+  clearInterval(timer);
+  timer = setInterval(nextImage, 5000);
 }
 
-document.addEventListener('keydown', function(event) {
-  switch (event.code){
-	  case "Space": case "ArrowRight":
-		nextImage();
-		break;
-	  case "ArrowLeft":
-		lastImage();
-		break;
+document.addEventListener("keydown", function (event) {
+  switch (event.code) {
+    case "Space":
+    case "ArrowRight":
+      nextImage();
+      break;
+    case "ArrowLeft":
+      lastImage();
+      break;
   }
 });
 
-window.onunload = function(event){
-	localStorage.setItem('count',count);
-}
+window.onunload = function (event) {
+  localStorage.setItem("count", count);
+};
